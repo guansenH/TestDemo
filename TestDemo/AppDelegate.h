8d151b8316a15db72a20d8e770fb40ed3a2967b4
冠森 huang <Hgs@huanggusendembp.lan>
2018-03-30 08:58:21 +0800

    Initial Commit

---
+++ TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by huang冠森 on 2018/3/30.
+//  Copyright © 2018年 HGS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by huang冠森 on 2018/3/30.
+//  Copyright © 2018年 HGS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
